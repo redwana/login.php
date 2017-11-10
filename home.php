d2a89489e9950836a61743804ff54f2914e06d08
Ralph Mujar <33500126+ralphmujar@users.noreply.github.com>
2017-11-10 14:06:10 +0800

    Login form
    Login forrm

---
+++ home.php
@@ -0,0 +1,50 @@
+
+
+</html>
+
+<head>
+<title> login form </title>
+<link rel = "stylesheet" href = "fv.css">
+</head>
+
+<body>
+
+
+<h2> <center> signup and login system </center> </h2>
+<br>
+
+
+
+<div id = "login">
+<form action = "prlog.php" method = "Post">
+username: <input id = "uid" name = "uid">
+password: <input id = "pwd" name = "pwd" type = "password">
+<button id = "btn" name = "btn1" > login </button>
+</form>
+</div>
+<br>
+<br>
+<br>
+
+
+
+
+<div id = "n">
+<ul>
+<li>home</li>
+<li><a href = "login.php">sign up </a></li>
+</ul>
+</div>
+<br>
+<br>
+<br>
+
+
+
+<script src = "fv.js"></script>
+
+
+</body>
+
+</html>
+
