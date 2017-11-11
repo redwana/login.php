5c4d84c10ed983af4b2e85e546c042aedf76f031
Ralph Mujar <33500126+ralphmujar@users.noreply.github.com>
2017-11-11 16:45:25 +0800

    Add files via upload

---
+++ prlog.php
@@ -0,0 +1,32 @@
+<?php
+
+
+session_start();
+
+
+$u = $_POST['uid'];
+$p = $_POST['pwd'];
+
+
+include 'dbc.php';
+
+$q = "select * from user where username = '$u' and password = '$p'";
+$r = mysqli_query($c, $q);
+
+$row = mysqli_fetch_array($r);
+
+if($row['username'] == $u && $row['password'] == $p){
+
+$_SESSION['up'] = $u;
+
+header("location: ../profile.php?login = success");
+
+}else{
+
+header("location: ../login.php?login = error");
+
+}
+
+
+
+?>
---
+++ profile.php
@@ -0,0 +1,51 @@
+<?php
+
+session_start();
+
+?>
+
+</html>
+
+<head>
+<title> login form </title>
+<link rel = "stylesheet" href = "fv.css">
+</head>
+
+<body>
+
+
+<h2> <center> signup and login system </center> </h2>
+<br>
+
+
+
+
+
+
+
+<div id = "n">
+<ul>
+<li>home</li>
+</ul>
+</div>
+<br>
+<br>
+<br>
+
+
+<?php
+
+echo "welcome ".$_SESSION['up'];
+?>
+
+
+
+
+
+<script src = "fv.js"></script>
+
+
+</body>
+
+</html>
+
